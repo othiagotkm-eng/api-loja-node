8d9d1a8db784c7df457eda1014f4b87b044e00d2
Thiago Silva <othiagotkm@gmail.com>
2026-07-02 15:07:32 -0300

    Aula 2 - rota de produtos

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ app.get("/", (req, res) => {
   res.send("Minha primeira API!");
 });
 
+app.get("/produtos", (req, res) => {
+       res.json([
+        {
+
+          id: 1,
+          nome:"mouse gamer",
+          preco:120
+
+        },
+        {
+          id: 2,
+          nome: "teclado macânico",
+          preco: 300
+
+        },
+       {
+            id: 3,
+            nome: "Monitor",
+            preco: 1200
+        }
+
+       ]);
+});
+
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
 });
