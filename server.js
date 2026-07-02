4d1abd0f0e4caccedc94ab5a590c59b7b8f94ca6
othiagotkm-eng <othiagotkm@gmail.com>
2026-07-01 22:41:51 -0300

    Primeira aula - API Node.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Minha primeira API!");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
