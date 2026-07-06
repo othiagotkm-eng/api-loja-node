0a145667af9e1366a442bf11a8b368d03d7d0864
Thiago Silva <othiagotkm@gmail.com>
2026-07-06 15:30:23 -0300

    rota POST e testes com o Postman

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Minha primeira API!");
+  res.send("Minha primeiraaa API!");
 });
 
 app.get("/produtos", (req, res) => {
@@ -30,6 +32,16 @@ app.get("/produtos", (req, res) => {
        ]);
 });
 
+app.post(("/produtos"), (req, res) => {
+
+  const produto = req.body;
+
+  res.json({
+    mensagem: "Produto recebido com sucesso",
+    produto: produto
+  })
+});
+
 app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+  console.log("Servidor rodando em http://localhost:3000/produtos");
 });
